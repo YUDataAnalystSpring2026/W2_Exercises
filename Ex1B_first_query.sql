19855e697e480cde217e300aa2ee6285867970c3
elizabeas <bess@rcc.team>
2026-02-16 22:41:25 -0600

    Added queries of actor and film tables

--- Ex1B_first_query.sql
+++ Ex1B_first_query.sql
@@ -2,3 +2,6 @@
 Sakila is a database used to collect information about ______ and ______ for use by ______.
 */
 
+SELECT * FROM actor; -- Retrieved 200 records
+
+SELECT * FROM film; -- Retrieved 1000 records
